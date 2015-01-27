67720ce0b481cbc8f918f2a7d5c430db88b7a9c4
Wayne <enyaw209@gmail.com>
2015-01-27 14:59:23 +0800

    many time...

--- a.c
+++ a.c
@@ -1,20 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  a.c
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  01/26/2015 03:22:49 PM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  YOUR NAME (), 
- *   Organization:  
- *
- * =====================================================================================
- */
 #include <stdlib.h>
 #include <stdio.h>
 
---
+++ hellow.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hellow.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/27/2015 01:57:48 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+
+int maim(){
+    printf("hello!!\n");
+    return 0;
+}
